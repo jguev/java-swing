6c792f132b7e7979a11795b3ec0b688567f837ee
Johanna <johannaguevara17@gmail.com>
2021-09-20 08:46:16 -0700

    Reorganized the directory

---
+++ Calculator/src/CalculatorGUI.java
@@ -0,0 +1,9 @@
+
+// Author: Johanna Guevara
+// Project Description:
+// This is an exercise that demonstrates how to create a simple GUI that uses a non-default layout manager,
+// and how to use a simple state machine to keep track of buttons pushed
+// GUI will use a 5-by-4 GridLayout with 5-pixel gaps between cells.
+
+public class CalculatorGUI {
+}
